416db8c65a1347b46a43f77ed05222f0e4307164
Pierre <pierre.nogaro@gmail.com>
2025-08-01 08:32:12 +0200

    fix nav

--- src/components/NavComponent.js
+++ src/components/NavComponent.js
@@ -1,29 +1,28 @@
 class NavComponent extends HTMLElement {
-    constructor() {
-        super();
-    }
-
     connectedCallback() {
         this.render();
     }
 
     render() {
         this.innerHTML = `
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container">
-                    <a class="navbar-brand" href="#">Pierre Nogaro</a>
+                    <a class="navbar-brand text-white fw-bold" href="#">Pierre Nogaro</a>
                     
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav ms-auto">
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Projects</a>
+                                <a class="nav-link text-white" href="#projects">Projects</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Contact</a>
+                                <a class="nav-link text-white" href="#contact">Contact</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="#about">About</a>
                             </li>
                         </ul>
                     </div>
